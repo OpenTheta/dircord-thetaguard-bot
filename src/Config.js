04da99f227039006e96473dfc469ba9a6cd4c6da
Simon Piazolo <53330806+Pizajolo@users.noreply.github.com>
2026-01-04 12:18:15 +0100

    Update VERIFY_BASE_URL in Config.js to point to the new production URL for the tag guard service, enhancing the application's deployment configuration.

--- src/Config.js
+++ src/Config.js
@@ -4,7 +4,7 @@ const FOOTER_TEXT = "**Make sure you sign the EXACT message and NEVER share your
 const DESCRIPTION = "You should expect to sign the following message when prompted by a non-custodial wallet such as MetaMask:"
 const LOGO_URL = "https://images.opentheta.io/creators/opentheta.jpg"
 // const VERIFY_BASE_URL = "http://localhost:3000/thetaguard/"
-const VERIFY_BASE_URL = "https://next.opentheta.io/thetaguard/"
+const VERIFY_BASE_URL = "https://opentheta.io/thetaguard/"
 const API_BASE_URL = process.env.API_BASE_URL || "https://api.opentheta.io/v1/"
 
 let USER_REQUESTS = {}
